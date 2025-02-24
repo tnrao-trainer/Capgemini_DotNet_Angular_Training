b9df4d81c84abbf8b5982eeb31f617762535a791
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-02-24 17:58:23 +0530

    Add files via upload

---
+++ 02.Day2_Angular_Components_Templates/Examples/Eg3_switch-demo_Directives/switch-demo.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switch-demo',
+  templateUrl: './switch-demo.component.html',
+  styleUrl: './switch-demo.component.css'
+})
+export class SwitchDemoComponent {
+
+  displayMode:string  = "Table";
+
+    public  empsArray:any[]  =  [];
+    
+   /*
+   public  empsArray:any[]  =  [
+    {  empno : 10256, ename:"Scott", job: "Manager", deptno : 10  },
+    {  empno : 10257, ename:"Smith", job: "Lead", deptno : 20  },
+    {  empno : 10258, ename:"Sandy", job: "Programmer", deptno : 30  },
+    {  empno : 10259, ename:"Sam", job: "Tester", deptno : 40  }, 
+    {  empno : 10256, ename:"Scott", job: "Manager", deptno : 10  },
+    {  empno : 10257, ename:"Smith", job: "Lead", deptno : 20  },
+    {  empno : 10258, ename:"Sandy", job: "Programmer", deptno : 30  },
+    {  empno : 10259, ename:"Sam", job: "Tester", deptno : 40  }, 
+    {  empno : 10256, ename:"Scott", job: "Manager", deptno : 10  },
+    {  empno : 10257, ename:"Smith", job: "Lead", deptno : 20  },
+    {  empno : 10258, ename:"Sandy", job: "Programmer", deptno : 30  },
+    {  empno : 10259, ename:"Sam", job: "Tester", deptno : 40  }
+  ];		
+*/
+}
